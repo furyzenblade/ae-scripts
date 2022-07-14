3d3377b9c020276983f8caed2a67f9b4e1648f50
furyzenblade <38463479+furyzenblade@users.noreply.github.com>
2022-07-14 18:53:46 +0200

    Add support for multiple layers

--- CreateTrimmedNulll.jsx
+++ CreateTrimmedNulll.jsx
@@ -1,46 +1,31 @@
-//testing();
-main();
+(function(){
+    function BindNullLayer(nullLayer, layer) {
+        nullLayer.moveBefore(layer);  // move null above layer
+        layer.parent = nullLayer;     // parent null to layer
+        nullLayer.threeDLayer = layer.threeDLayer;  //copy 3d attribute
 
-function CreateTrimmedNull(comp, layer) {
-    app.beginUndoGroup("Create New Null Layer"); // create new undo group
-
-    var nullLayer = comp.layers.addNull(); // reate new null
-    nullLayer.moveBefore(layer); // move null above layer
-    layer.parent = nullLayer; // parent null to layer
-    nullLayer.threeDLayer = layer.threeDLayer; //copy 3d attribute
-
-    // copy inPoint and outPoint
-    nullLayer.inPoint = layer.inPoint;
-    nullLayer.outPoint = layer.outPoint;
-
-    // reopen edited properties
-    //app.executeCommand(2065);
-
-    app.endUndoGroup(); //end the undo group
-}
-
-function main() {
-    // get selected comp
-    var comp = app.project.activeItem;
-    if (!isValid(comp)){
-        alert("Please select a comp");
+        // copy inPoint and outPoint
+        nullLayer.inPoint = layer.inPoint;
+        nullLayer.outPoint = layer.outPoint;
+    }
+    //
+    //
+    var comp = app.project.activeItem; // get selected comp
+    if (comp == null || !(comp instanceof CompItem)) {
+        alert("Select a comp");
         return;
     }
 
-    // get selected layer
-    var selectedLayers = comp.selectedLayers;
-    if (comp.selectedLayers.length != 1) {
-        alert("Please select one layer");
+    var selectedLayers = comp.selectedLayers; // get selected layers
+    if (selectedLayers.length <= 0){
+        alert("Select at least one layer");
         return;
     }
-    var selectedLayer = selectedLayers[0];
 
-    CreateTrimmedNull(comp, selectedLayer);
-}
-
-function testing(){
-    //var cmd = app.findMenuCommandId("Position...");
-    //alert(cmd);
-    app.executeCommand(2771);
-    return;
-}
+    app.beginUndoGroup("Create Trimmed Null Layer");
+    for (var i = 0; i < selectedLayers.length; i++) {
+        var layer = selectedLayers[i];
+        BindNullLayer(comp.layers.addNull(), layer);
+    }
+    app.endUndoGroup();
+})();
